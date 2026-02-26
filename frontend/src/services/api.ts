f140f365b6acf7fb559ec29b8cd9c083bbde68e8
Junior Ramos <ramosflores135@gmail.com>
2026-02-26 09:23:11 -0500

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://logintest-oqud.onrender.com";
 
 export async function login(usuario: string, password: string) {
   const response = await fetch(`${API_URL}/login`, {
