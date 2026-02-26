66f46c131a612faca84465c07c7c3a139741169b
Junior Ramos <ramosflores135@gmail.com>
2026-02-26 08:33:36 -0500

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:arrozconhuebo10*@db.aoecrandhdnexxwhbdue.supabase.co:5432/postgres"
+DATABASE_URL = "postgresql://neondb_owner:npg_JQCdSmFzyr59@ep-falling-bird-ackxbs2r-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
